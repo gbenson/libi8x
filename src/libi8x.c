5d9b9ef0e6eccc332cc213149281ffbed9755c26
Gary Benson <gbenson@redhat.com>
2016-03-17 13:17:31 +0000

    libi8x: Replace boilerplate

--- src/libi8x.c
+++ src/libi8x.c
@@ -1,18 +1,21 @@
-/*
-  i8x - something with i8x
+/* Copyright (C) 2016 Red Hat, Inc.
+   This file is part of the Infinity Note Execution Library.
 
-  Copyright (C) 2011 Someone <someone@example.com>
+   The Infinity Note Execution Library is free software; you can
+   redistribute it and/or modify it under the terms of the GNU Lesser
+   General Public License as published by the Free Software
+   Foundation; either version 2.1 of the License, or (at your option)
+   any later version.
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
+   The Infinity Note Execution Library is distributed in the hope that
+   it will be useful, but WITHOUT ANY WARRANTY; without even the
+   implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+   PURPOSE.  See the GNU Lesser General Public License for more
+   details.
 
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-*/
+   You should have received a copy of the GNU Lesser General Public
+   License along with the Infinity Note Execution Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
 
 #include <stdio.h>
 #include <stdlib.h>
