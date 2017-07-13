f39e66c529eab09c645a9c716e2ad894032ff0be
Gary Benson <gbenson@redhat.com>
2017-07-13 14:47:55 +0100

    test_py8x_inf_set_read_mem_fn.py: Merge into test_py8x_read_mem_fn.py

--- python/tests/lo/test_py8x_read_mem_fn.py
+++ python/tests/lo/test_py8x_read_mem_fn.py
@@ -25,9 +25,25 @@ from __future__ import unicode_literals
 
 import _libi8x as py8x
 from . import common
+import struct
 
 class TestPy8xReadMemFn(common.PopulatedTestCase):
     TESTNOTE = common.PopulatedTestCase.DEREF_NOTE
+
+    def test_no_readmem_func(self):
+        """Test py8x_read_mem_fn with no memory reader function."""
+        self.assertRaises(py8x.I8XError,
+                          py8x.xctx_call,
+                          self.xctx, self.funcref, self.inf, (5,))
+
+    def test_read_memory(self):
+        """Test reading memory."""
+        STORAGE = b"HeLlOmUm"
+        def readmem(inf, addr, len):
+            return STORAGE[addr:addr + len]
+        py8x.inf_set_read_mem_fn(self.inf, readmem)
+        self.assertIn(py8x.xctx_call(self.xctx, self.funcref, self.inf, (3,)),
+                      [struct.unpack(fmt, b"lOmU") for fmt in (b">I", b"<I")])
 
     def test_bad_argc(self):
         """Check py8x_read_mem_fn catches wrong numbers of arguments."""
